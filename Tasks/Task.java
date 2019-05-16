e17c5da8f5719eeb24b58d230954b854fd514618
Alex-Holborn <alexander.holborn@my.jcu.edu.au>
2019-05-16 20:57:38 +1000

    add randomReturnDelay add getRandomReturnDelay

--- Tasks/Task.java
+++ Tasks/Task.java
@@ -4,11 +4,24 @@ public abstract class Task implements ITask{
 
     private Main main;
 
-    Task(Main m){
+    private int[] randomReturnDelay;
+
+    Task(Main m, int[] delay){
         main = m;
+        setRandomReturnDelay(delay);
     }
 
     Main getBot(){
         return main;
+    }
+
+    public int[] getRandomReturnDelay(){
+        return randomReturnDelay;
+    }
+
+    private void setRandomReturnDelay(int[] delay){
+        randomReturnDelay = new int[2];
+        randomReturnDelay[0] = delay[0];
+        randomReturnDelay[1] = delay[1];
     }
 }
