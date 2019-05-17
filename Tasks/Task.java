d50113cd43bcee8e01c6ff82d90edf3484422863
Alex-Holborn <alexander.holborn@my.jcu.edu.au>
2019-05-17 20:58:45 +1000

    add exitMessage

--- Tasks/Task.java
+++ Tasks/Task.java
@@ -1,13 +1,13 @@
 package Tasks;
 import main.Main;
 
-import java.awt.*;
-
 public abstract class Task implements ITask{
 
     private Main main;
 
     private int[] randomReturnDelay;
+
+    private String exitMessage = "Thank you for using an LxBot!";
 
     Task(Main m, int[] delay){
         main = m;
@@ -26,6 +26,14 @@ public abstract class Task implements ITask{
         randomReturnDelay = new int[2];
         randomReturnDelay[0] = delay[0];
         randomReturnDelay[1] = delay[1];
+    }
+
+    public void finishBot(){
+        getBot().log(exitMessage);
+    }
+
+    void setExitMessage(String s){
+        exitMessage = s;
     }
 
 }
