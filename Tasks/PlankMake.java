6a7edbd107f10217b7356bdeddc011413af0351b
Alex-Holborn <alexander.holborn@my.jcu.edu.au>
2019-05-17 17:39:01 +1000

    add paintOverride

--- Tasks/PlankMake.java
+++ Tasks/PlankMake.java
@@ -1,16 +1,22 @@
 package Tasks;
-
 import main.Main;
+
+import java.awt.*;
 
 public class PlankMake extends Task {
 
-    public PlankMake(Main m){
-        super(m);
+    public PlankMake(Main m, int[] delay){
+        super(m, delay);
     }
 
     @Override
     public void runTask(){
         getBot().log("Running Plank Make...");
+    }
+
+    @Override
+    public void paintOverride(Graphics2D g) {
+
     }
 
 }
--- Tasks/SuperglassMake.java
+++ Tasks/SuperglassMake.java
@@ -1,15 +1,20 @@
 package Tasks;
-
 import main.Main;
+import java.awt.*;
 
 public class SuperglassMake extends Task{
 
-    public SuperglassMake(Main m) {
-        super(m);
+    public SuperglassMake(Main m, int[] delay) {
+        super(m, delay);
     }
 
     @Override
     public void runTask(){
         getBot().log("Running SGM");
+    }
+
+    @Override
+    public void paintOverride(Graphics2D g) {
+
     }
 }
